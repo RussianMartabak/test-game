472970b77294e7e268e590548610f61e0bb7a591
Zaky <zakyabdurrahman10@gmail.com>
2022-01-02 11:52:16 +0700

    added readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Test Game
+Make sure Node.js is installed then type `http-server` inside the folder to run it
--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-const app = new PIXI.Application({width: 500, height: 500});
+//aliases
+const Application = PIXI.Application,
+    loader = PIXI.Loader.shared,
+    resources = PIXI.Loader.shared.resources,
+    Sprite = PIXI.Sprite;
+
+
+const app = new Application({width: 500, height: 500});
 console.log(app);
+
 document.body.appendChild(app.view);
+
+//load an image to WebGL texture
+PIXI.Loader.shared
+    .add('assets/tank.png')
+    .load(setup);
+
+//what to do when loaded
+function setup() {
+    let tank = new PIXI.Sprite(
+        PIXI.Loader.shared.resources['assets/tank.png'].texture
+    );
+    
+    tank.width = 50;
+    tank.height = 50;
+    app.stage.addChild(tank);
+}
