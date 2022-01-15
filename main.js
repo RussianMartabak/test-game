e69c674071df692193b55731f84441b525dd8559
Zaky <zakyabdurrahman10@gmail.com>
2022-01-16 05:57:32 +0700

    Added new assets and texture atlas for fast loading
    added two new sprites that is loaded using a texture atlas in the form
    of json

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ loader.onProgress.add(loading);
 //load an image to WebGL texture
 loader
     .add('assets/tank.png')
+    .add('assets/soviet.json')
     .load(setup);
 
 //log when loading
@@ -28,15 +29,19 @@ function setup() {
     let tank = new PIXI.Sprite(
         resources['assets/tank.png'].texture
     );
-    
+    const id = resources['assets/soviet.json'].textures;
+    const background = new Sprite(id['snowbg.png']);
+    const sovSoldier = new Sprite(id['soviet-soldier.png']);
+
+    app.stage.addChild(background);    
     tank.width = 100;
     tank.height = 100;
-    tank.position.set(150, 150);
+    tank.position.set(100, 250);
     app.stage.addChild(tank);
     tank.anchor.set(0.5, 0.5);
-    rotateForever(tank);
+    
 }
-
-function rotateForever(sprite){
-    setInterval(() => sprite.rotation += 0.1, 100);
-}
+//either this or setTimeout but the dlay multiplied by the limit to create delayed loop
+//function rotateForever(sprite){
+//    setInterval(() => sprite.angle += 10, 100);
+//}
